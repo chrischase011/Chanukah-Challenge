020c456d50098316df55242f9fc3e3b8d20819ae
Christopher Robin Chase <43966456+chrischase011@users.noreply.github.com>
2021-11-11 12:03:24 +0800

    Create chanukah.py

---
+++ chanukah.py
@@ -0,0 +1,22 @@
+# Chanukah Challenge solved by Christopher Robin Chase 💖
+def main(n = int(input())):
+    
+    while(n != 0):
+        ans = 0
+        c = 0
+        s = input()
+        i, t = s.split()
+        x = int(t)
+        for b in range(x):
+            if(x == 1):
+                ans = x + 1
+                break
+            b = b + 1
+            c = (c + b)
+            ans = c + x
+        
+        print(i, ans)
+        n = n - 1
+
+
+main()
